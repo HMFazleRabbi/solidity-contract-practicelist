ac7bed9fbc57345083afc30f2397d1402c88d9a4
HMFazleRabbi <f.rabbiarcs@gmail.com>
2023-09-19 16:57:25 +0600

    handle errors in contract

--- scripts/errortest.ts
+++ scripts/errortest.ts
@@ -1,27 +1,5 @@
 import { ethers } from "hardhat";
 
-// Function to handle the vote and catch errors
-async function vote(
-  inFavor: boolean,
-  contract: { vote: (arg0: any) => any } | undefined
-) {
-  try {
-    // Call the vote function
-    const tx = await contract.vote(inFavor);
-
-    // Wait for the transaction to be mined
-    await tx.wait();
-
-    // Transaction successful
-    console.log("Transaction successful.");
-  } catch (error) {
-    if (error.message.includes("AlreadyVoted")) {
-      console.error("Error: You have already voted.");
-    } else {
-      console.error("Error:", error.message);
-    }
-  }
-}
 
 async function main() {
   const [deployer, addr1, addr2] = await ethers.getSigners();
